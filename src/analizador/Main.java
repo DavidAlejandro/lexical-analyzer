ff6220e93d9a2d6db4b79978e06828e869b396c8
DavidAlejandro <davidporcel@me.com>
2015-02-22 18:30:24 -0600

    empezando

---
+++ src/analizador/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package analizador;
+
+import java.io.File;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String path = "/Users/David/NetBeansProjects/analizadorLexico-Java/src/analizador/Lexer.flex";
+        generarLexer(path);
+
+    }
+    public static void generarLexer(String path){
+        File file=new File(path);
+        JFlex.Main.generate(file);
+    }
+}
